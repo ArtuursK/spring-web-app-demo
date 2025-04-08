d7bc2b420156293c688784868c0514d364c1820b
IZV\adm_ak <arturs@arturs.com>
2025-04-08 17:06:26 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpRequest;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String returnText() {
+        return "Hello";
+    }
+
+    @RequestMapping(value = "/{city}")
+    @ResponseBody
+    public String getTemperature() {
+        //fetch the temperature from api (GET request)
+        //get JSON back in a String
+        //parse the JSON
+        return json("temperature");
+    }
+
+    @RequestMapping(value = "/color", method = RequestMethod.GET)
+    @ResponseBody
+    public String getTemperature2(
+            @RequestParam("c") String color //query parameters
+    ) {
+        return color;
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    @ResponseBody
+    public String getTemperature3(
+            @ModelAttribute TemperatureInput temperatureInput
+    ) {
+//        {
+//            "location":"Riga",
+//            "date":"08-04-2025"
+//        }
+
+        temperatureInput.date
+        temperatureInput.location
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo/TemperatureInput.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class TemperatureInput {
+    String location;
+    Date date;
+}
